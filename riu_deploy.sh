c8c3974778d507fef3d723469b9f1933ebeef719
ricardo <ricardo.david.rd@gmail.com>
2019-02-19 15:40:59 -0500

    getting rid of sh

--- riu_deploy.sh
+++ riu_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "ricardo";
 pwd;
-cp .env.example .env;
-composer install;
-composer  update;
+#cp .env.example .env;
+#composer install;
+#composer  update;
 echo "about to get key";
-php artisan key:generate;
+#php artisan key:generate;
